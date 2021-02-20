d574a6df401b6b5bfab501ef37a6c2960a5685c8
Ethan Fann <ecfann@gmail.com>
2021-02-20 12:23:41 -0800

    Initial Commit

---
+++ avarts-widget/avarts_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  avarts_widgetApp.swift
+//  avarts-widget
+//
+//  Created by Ethan Fann on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct avarts_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
